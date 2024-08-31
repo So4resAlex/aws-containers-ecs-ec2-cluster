77ed92fffe4ba0b8da7d10ac704353391f9c25e5
Alex_Soares <alealvessoares28@gmail.com>
2024-08-31 20:16:32 -0300

    chore: add variables to parameter store

--- variables.tf
+++ variables.tf
@@ -5,3 +5,38 @@ variable "region" {
 variable "project_name" {
   type = string
 }
+
+variable "ssm_vpc_id" {
+    type = string
+  
+}
+
+variable "ssm_public_subnet_1" {
+    type = string
+  
+}
+
+variable "ssm_public_subnet_2" {
+    type = string
+  
+}
+
+variable "ssm_public_subnet_3" {
+    type = string
+  
+}
+
+variable "ssm_private_subnet_1" {
+    type = string
+  
+}
+
+variable "ssm_private_subnet_2" {
+    type = string
+  
+}
+
+variable "ssm_private_subnet_3" {
+    type = string
+  
+}
